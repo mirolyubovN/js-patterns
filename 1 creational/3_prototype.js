adc0a7de93c46b131aad6e132d62ca5e3c0b9895
nikita.mirolyubov <nikita.mirolyubov@graph.uk>
2020-12-06 10:55:25 +0500

    creational design patterns

---
+++ 1 creational/3_prototype.js
@@ -0,0 +1,15 @@
+const car = {
+    wheels: 4,
+    init() {
+        console.log(`I have ${this.wheels} wheels. My owner is ${this.owner}`);
+    }
+};
+
+const carWithOwner = Object.create(car, {
+    owner: {
+        value: 'Nikita'
+    }
+});
+
+console.log(carWithOwner.__proto__ === car);
+carWithOwner.init()
